0c17172d9c0a05b86d5b920d1c8fa4c548cc4f9e
Sai Kumar <51734282+bhogi50@users.noreply.github.com>
2023-09-30 19:24:46 +0530

    Create 12646.cpp

---
+++ Uva/12646.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+int main()
+{
+    int a,b,c;
+    
+    while(scanf("%d %d %d", &a, &b, &c) != EOF)
+    {
+        if(a == b && a == c )
+        {
+            printf("*\n");
+        }
+        else
+        {
+            if(a==b && a!=c)
+            {
+                printf("C\n");
+            }
+            else if(a==c && a!=b)
+            {
+                printf("B\n");
+            }
+            else
+            {
+                printf("A\n");
+            }
+        }
+        
+    }
+
+    return 0;
+}
